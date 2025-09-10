20f5f8b4c38331ec588dfdef5e126f6fdcefe3e2
suprava <supravaswn@gmail.com>
2025-09-10 22:37:36 +0530

    initial commit

---
+++ MicroservicesWithConfigServer/Micro-Calldetails/src/main/java/com/micro/calldetails/MicroCalldetailsApplication.java
@@ -0,0 +1,15 @@
+package com.micro.calldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+
+public class MicroCalldetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCalldetailsApplication.class, args);
+	}
+
+}
